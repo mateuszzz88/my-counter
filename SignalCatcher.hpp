b00e8b284cf54e8b6d431fd4eac975b82e887d19
Przemys?aw Ogorza?ek <przemyslaw.ogorzalek@gmail.com>
2011-05-15 13:50:20 +0200

    Dopisane funkcje przechwytuj?ce sygna?y pauzy. Nie s? na razie u?ywane, bo nie wiem teraz czy jest sens robi? osobny w?tek do sterowania.

--- SignalCatcher.hpp
+++ SignalCatcher.hpp
@@ -5,10 +5,34 @@
 bool SIGINT_sent = 0;
 
 /**
+ * Zmienna globalna przechowująca informację o zapauzowaniu programu
+ */
+
+bool SIGTSTP_sent = 0;
+/**
  * Funkcja globalna przechwytująca sygnał SIGINT i zmieniająca wartość zmiennej SIGINT_sent na jeden, co pozwala odczytać później jej stan.
  */
 void catch_int(int sig_num)
 {
  signal(SIGINT, catch_int);
  SIGINT_sent = 1;
+}
+
+/**
+ * Funkcja globalna przechwytująca sygnał SIGTSTP i zmieniająca wartość zmiennej SIGTSTP na przeciwną
+ */
+void catch_tstp(int sig_num)
+{
+ signal(SIGTSTP, catch_tstp);
+ SIGTSTP_sent = !SIGTSTP_sent;
+}
+
+/**
+ * Funkcja globalna przechwytująca sygnał SIGCONT i ustawiająca wartość zmiennej SIGTSTP_sent na 0
+ */
+
+void catch_cont(int sig_num)
+{
+ signal(SIGCONT, catch_cont);
+ SIGTSTP_sent = 0;
 }
