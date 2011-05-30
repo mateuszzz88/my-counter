aaf10e3274cd0bb9780dac2bc9ad477c39844fc0
Mateusz Lewicki <mateuszzz88@gmail.com>
2011-05-31 01:15:54 +0200

    some code reformatting and minor cleaning

--- Test.cpp
+++ Test.cpp
@@ -14,71 +14,51 @@ using namespace std;
 
 void manualTest();
 
-void onSignalStepDone() {cout <<"."<<flush;}
+void onSignalStepDone()
+{
+    cout << "." << flush;
+}
 
 int main()
 {
-  
-  manualTest();
-  return 0;
-  
-  /*
-  std::ofstream ofs("filename", ios::binary);
-  DataStruct ds(35, 1);
-  
-  CounterBase<DataStruct> g(ds);
-//   g.startCalculations();
-//   sleep(10);
-//   g.stopCalculations();
-  g.doCalculations();
-  
- 
-  DataStruct sd(0,0); 
-  CounterBase<DataStruct>  newg(sd);
-  std::ifstream ifs("filename", ios::binary);
-    {
-	boost::archive::text_iarchive ia(ifs);
-	ia >> newg;
-        newg.d();
-    }
 
- return 0;*/
+    manualTest();
+    return 0;
 }
 
+void manualTest()
+{
+    BigPrimeData ds;
+    myExampleTaskA g(ds, 5);
+    //myExampleTaskA g; //takes default constructor of BigPrimeData
+    g.setSerializationFile("sfilename", true);
+    //  g.addSlotStepDone(boost::bind(onSignalStepDone)); //TODO why doesn't work??
+    //  g.addSlotStepDone(onSignalStepDone);
+    string avilableCommands =
+            "avilable commands: \n"
+            "help      - shows this list, \n"
+            "exit      - closes test program, \n"
+            "start_syn - starts calculations in main thread, \n"
+            "start     - creates calculations thread, \n"
+            "join      - waits for calculations thread to finish, \n"
+            "stop      - stops calculations thread\n";
+    cout << avilableCommands;
+    string input;
 
-
-void manualTest(){
-  BigPrimeData ds;
-  myExampleTaskA g(ds, 5);
-  //myExampleTaskA g; //takes default constructor of BigPrimeData
-  g.setSerializationFile("sfilename",true);
-//  g.addSlotStepDone(boost::bind(onSignalStepDone)); //TODO why doesn't work??
-//  g.addSlotStepDone(onSignalStepDone);
-  string avilableCommands =
-          "avilable commands: \n"
-          "help      - shows this list, \n"
-          "exit      - closes test program, \n"
-          "start_syn - starts calculations in main thread, \n"
-          "start     - creates calculations thread, \n"
-          "join      - waits for calculations thread to finish, \n"
-          "stop      - stops calculations thread\n";
-  cout <<avilableCommands;
-  string input;
-  
-  while (1) {
-    cin>>input;
-    if (input=="exit" || input=="quit") {
-      return;
-    } else if (input=="start") {
-      g.startCalculations();
-    } else if (input=="stop"){
-      g.stopCalculations();
-    } else if (input=="start_syn"){
-      g.doCalculations();
-    } else if (input=="join"){
-      g.join();
-    } else {
-      cout <<"command not understood: "<<input <<endl <<avilableCommands <<flush;
-	}
-  }
+    while (1) {
+        cin >> input;
+        if (input == "exit" || input == "quit") {
+            return;
+        } else if (input == "start") {
+            g.startCalculations();
+        } else if (input == "stop") {
+            g.stopCalculations();
+        } else if (input == "start_syn") {
+            g.doCalculations();
+        } else if (input == "join") {
+            g.join();
+        } else {
+            cout << "command not understood: " << input << endl << avilableCommands << flush;
+        }
+    }
 }
