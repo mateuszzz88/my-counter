982a156857655ce06ebe0887df7f39e449790342
Przemys?aw Ogorza?ek <przemyslaw.ogorzalek@gmail.com>
2011-05-31 23:00:55 +0200

    Plik tex

--- tests/myExampleTaskA.cpp
+++ tests/myExampleTaskA.cpp
@@ -1,32 +1,32 @@
+/* 
+ * File:   myExampleTaskA.cpp
+ * Author: mateuszzz
+ * 
+ * Created on 28 maj 2011, 15:21
+ */
+
+
 #include "myExampleTaskA.h"
 #include <math.h>
 
-// 3 implement worker class's Calculate function
 template <>
-void CounterBase<BigPrimeData>::Calculate()
-{
-    // 3c. inspect and modify data property in any way necessary to complete task
-    long long beingChecked = data().lastChecked + 1;
+void CounterBase<BigPrimeData>::Calculate() {
+    long long beingChecked = *(data.lastChecked) + 1;
     bool isPrime = true;
-    for (int i = 2; i <= sqrt(beingChecked); ++i) {
-        if (beingChecked % i == 0) {
+    for (int i=2; i<=sqrt(beingChecked); ++i){
+        if (beingChecked%i==0){
             isPrime = false;
             break;
         }
     }
-    data().lastChecked = beingChecked;
+    *(data.lastChecked) = beingChecked;
+    std::cout << *(data.lastChecked) << std::endl;
     if (isPrime) {
-        data().lastFound = beingChecked;
+        data.lastFound = beingChecked;
     }
-
-    //sleep() is needed only for presentational purposes - allows to
-    // predict outcome of calculations after given time.
-    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-    
-    // 3a when final outcome is achieved, call setFinished(true) to notify
-    // all interested about finished task and stop calculation loop.
-    // This step is optional, calculations can be expected to be stopped manually.
-    if (data().lastChecked >= data().maxChecked)
-        setFinished(true);
+    //TODO when to end?
+    //Answer: Kiedy skończy się zakres long long
+    //Przykładowo:
+    //if (beingChecked == 9223372036854775806) stop = true;
 }
 
