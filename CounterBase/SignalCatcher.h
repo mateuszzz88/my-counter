501d48d0654f4d055806d771c229d5accd9905ee
Przemys?aw Ogorza?ek <przemyslaw.ogorzalek@gmail.com>
2011-06-01 00:21:39 +0200

    Dodane na nowo sygna?y

---
+++ CounterBase/SignalCatcher.h
@@ -0,0 +1,51 @@
+#ifndef SIGHNALCATHER_H
+#define SIGHNALCATHER_H
+#include<signal.h>
+
+/**
+ * Global variable, which stores information about SIGINT and SIGTERM signal status
+ */
+
+extern bool SIGINTorTERM_sent;
+
+/**
+ * Global variable, which stores information about SIGTSTP and SIGCONT signals status
+ */
+
+extern bool SIGTSTP_sent;
+
+/**
+ * Global variable, which stores information about SIGUSR1 signal status
+ */
+
+extern bool SIGUSR1_sent;
+
+/**
+ * Global function. Catches SIGINT signal and sets SIGINT_sent value to 1.
+ */
+void catch_int(int sig_num);
+
+/**
+ * Global function. Catches SIGTERM signal and sets SIGINT_sent value to 1.
+ * Identical to catch_int function.
+ */
+void catch_term(int sig_num);
+
+/**
+ * Global function. Catches SIGTSTP signal and sets SIGTSTP_sent value to opposite.
+ */
+void catch_tstp(int sig_num);
+
+/**
+ * Global function. Catches SIGCONT signal and sets SIGTSTP_sent value to 0.
+ */
+
+void catch_cont(int sig_num);
+
+/**
+ * Global function. Catches SIGUSR1 signal and sets SIGUSR1_sent value to 1.
+ */
+void catch_usr1(int sig_num);
+
+
+#endif //SIGHNALCATHER_H
---
+++ tests/myExampleTaskA.h
@@ -0,0 +1,64 @@
+/* 
+ * File:   myExampleTaskA.h
+ *
+ * class shows example usage of MyCounter
+ */
+#ifndef MYEXAMPLETASKA_H
+#define	MYEXAMPLETASKA_H
+
+#include "../CounterBase/MyCounter.hpp"
+
+
+// 1. First create structure to store calculation data
+// In simple cases primitive types will do just fine
+
+struct BigPrimeData {
+    // 1a. Create as many fields as You like
+    long long lastChecked;
+    long long lastFound;
+    long long maxChecked;
+
+    // 1b. If default constructors are not enough, create your own.
+    // In simple cases shouldn't be necessary.
+
+    BigPrimeData() :
+    lastChecked(2),
+    lastFound(2),
+    maxChecked(100)
+    {}
+
+    /**
+     * Copy constructor - will be used!
+     */
+    BigPrimeData(const BigPrimeData & other) :
+    lastChecked(other.lastChecked),
+    lastFound(other.lastFound),
+    maxChecked(other.maxChecked)
+    {}
+
+    /**
+     * Function used in serialization proces;
+     * It's sufficient to list all structure's fields in format:
+     * ar & fieldName1;
+     * ar & fieldName2;
+     */
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version) {
+        ar & lastChecked;
+        ar & lastFound;
+        ar & maxChecked;
+    }
+};
+
+// 2. Now create worker class, derivate as shown
+
+// 2a reimplement Calculate if task can be divided into sequential steps
+template <>
+void CounterBase<BigPrimeData>::Calculate();
+
+// 2b typedef for easer usage
+typedef CounterBase<BigPrimeData> myExampleTaskA;
+    
+
+#endif	/* MYEXAMPLETASKA_H */
+
